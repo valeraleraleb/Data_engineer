06046e000389eb2d7ce5bbf2748d0c1359b3391b
Lera <88451135+valeraleraleb@users.noreply.github.com>
2025-03-14 16:25:26 +0100

    Create NFs.sql

---
+++ DWH/NFs.sql
@@ -0,0 +1,17 @@
+/* Создание схемы nf_lesson */
+create schema nf_lesson;
+
+/* Создание таблицы nf_lesson.preparatory_1_1nf */
+DROP TABLE IF EXISTS nf_lesson.preparatory_1_1nf;
+
+CREATE TABLE nf_lesson.preparatory_1_1nf AS
+SELECT
+	id -- идентификатор записи
+	, craftsman_name
+	, craftsman_address
+	, customer_name
+	, customer_address
+FROM source1.craft_market_wide;
+
+/* Задание первичного ключа таблицы */
+ALTER TABLE nf_lesson.preparatory_1_1nf ADD CONSTRAINT pk_preparatory_1_1nf PRIMARY KEY (id);
