cebb8c9906e37e15f6031e66dc94b31b934143ba
Lera <88451135+valeraleraleb@users.noreply.github.com>
2025-03-18 19:54:57 +0100

    Update SQL req.sql

--- Docker/Data_Mart/SQL req.sql
+++ Docker/Data_Mart/SQL req.sql
@@ -55,9 +55,96 @@ WHERE tc.constraint_type = 'FOREIGN KEY';
  using (client_id)
  limit 100;
 
--- 
+-- Выгрузите из таблицы user_activity_log первые 10 записей с идентификатором клиента, датой захода (дата события, если событие visit, и NULL иначе) и флагом регистрации (1, если событие registration, и 0 иначе).
+-- Метрики положите в новые столбцы visit_dt и is_registration.
+SELECT 
+client_id
+, case
+	when action = 'visit' then hitdatetime else NULL
+end as visit_dt
+, case
+	when action = 'registration' then 1 else 0
+end is_registration
+FROM public.user_activity_log
+limit 10;
 
+-- В конечной витрине данные нужны совокупно по каждому клиенту, поэтому метрики из предыдущего задания нужно агрегировать.
+-- Выгрузите первые 10 записей, в которых для каждого пользователя содержится:
+-- client_id — идентификатор клиента;
+-- fst_visit_dt — дата первого посещения сайта;
+-- registration_dt — дата регистрации. Если таких событий несколько, возьмите самое раннее;
+-- is_registration — флаг регистрации ( 1, если пользователь когда-либо зарегистрировался, и 0 иначе).
+-- Приведите колонки с датами к типу DATE.
 
+SELECT client_id,
+       DATE(MIN(CASE WHEN action = 'visit' THEN hitdatetime ELSE NULL END)) AS fst_visit_dt,
+       DATE(MIN(CASE WHEN action = 'registration' THEN hitdatetime ELSE NULL END)) AS registration_dt,
+       MAX(CASE WHEN action = 'registration' THEN 1 ELSE 0 END) AS is_registration
+FROM user_activity_log
+GROUP BY client_id
+LIMIT 10;
 
+-- Помимо данных по посещению и регистрации, в витрине необходима также информация о платежах и о маркетинговой кампании, чтобы проанализировать оборот. 
+-- Для этого нужно объединить данные из нескольких таблиц.
+-- Соберите все данные в витрину с полями:
+-- client_id — идентификатор клиента;
+-- utm_campaign — маркетинговая кампания;
+-- fst_visit_dt — дата первого посещения сайта;
+-- registration_dt — дата регистрации клиента;
+-- is_registration — 1, если клиент регистрировался, 0 иначе;
+-- total_payment_amount — сумма платежей клиента.
 
+SELECT ua.client_id,
+       ua.utm_campaign,
+       ual.fst_visit_dt,
+       ual.registration_dt,
+       ual.is_registration,
+       upl.total_payment_amount
+FROM user_attributes AS ua
+LEFT JOIN (
+       SELECT client_id,
+              DATE(MIN(CASE WHEN action = 'visit' THEN hitdatetime ELSE NULL END)) AS fst_visit_dt,
+              DATE(MIN(CASE WHEN action = 'registration' THEN hitdatetime ELSE NULL END)) AS registration_dt,
+              MAX(CASE WHEN action = 'registration' THEN 1 ELSE 0 END) AS is_registration
+       FROM user_activity_log
+       GROUP BY client_id
+) AS ual
+ON ua.client_id = ual.client_id
+LEFT JOIN (
+       SELECT client_id,
+              SUM(payment_amount) AS total_payment_amount
+       FROM user_payment_log
+       GROUP BY client_id
+) AS upl
+ON ua.client_id = upl.client_id
+limit 10;
 
+-- Запрос из предыдущего задания получился очень громоздкий. Читать его сложно и неудобно. 
+-- Сделайте запрос более понятным, вынеся все подзапросы в CTE.
+
+WITH ual AS (
+    SELECT client_id,
+              DATE(MIN(CASE WHEN action = 'visit' THEN hitdatetime ELSE NULL END)) AS fst_visit_dt,
+              DATE(MIN(CASE WHEN action = 'registration' THEN hitdatetime ELSE NULL END)) AS registration_dt,
+              MAX(CASE WHEN action = 'registration' THEN 1 ELSE 0 END) AS is_registration
+       FROM user_activity_log
+       GROUP BY client_id
+),
+
+upl AS (
+	 SELECT client_id,
+	              SUM(payment_amount) AS total_payment_amount
+	       FROM user_payment_log
+	       GROUP BY client_id
+)
+
+SELECT ua.client_id,
+       ua.utm_campaign,
+       ual.fst_visit_dt,
+       ual.registration_dt,
+       ual.is_registration,
+       upl.total_payment_amount
+FROM user_attributes AS ua
+LEFT JOIN ual using (client_id)
+LEFT JOIN upl using (client_id)
+limit 10;
